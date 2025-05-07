5a7e3f53bdb4d4b17163df02bcb4ae825468c7be
Raphael Campos Vieira <rapcampo@c2r1s6.42porto.com>
2025-05-07 19:38:03 +0100

    updated mlx

--- mlx/mlx_new_image.c
+++ mlx/mlx_new_image.c
@@ -23,9 +23,8 @@ int	mlx_X_error;
 
 int	shm_att_pb(Display *d,XErrorEvent *ev)
 {
-  int	r_value;
   if (ev->request_code==146 && ev->minor_code==X_ShmAttach)
-    r_value = write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
+    write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
   mlx_X_error = 1;
 }
 
