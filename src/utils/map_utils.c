4b8bab3e607e749a0849d54dc7516830fb2e47dc
Tiago <tfa.antunes@gmail.com>
2025-06-19 11:56:02 +0100

    fix: read in non allocated memory

--- src/utils/map_utils.c
+++ src/utils/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 17:33:23 by tialbert          #+#    #+#             */
-/*   Updated: 2025/06/15 15:26:50 by tialbert         ###   ########.fr       */
+/*   Updated: 2025/06/19 11:29:09 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ void	extend_map(size_t nb_lines)
 	char	**new_map;
 	size_t	i;
 
-	new_map = malloc(sizeof(char *) * (nb_lines + 2));
+	new_map = malloc(sizeof(char *) * (nb_lines + 3));
 	if (new_map == NULL)
 		exit_log("Error: Memory allocation error\n");	
 	i = -1;
 	while (++i < nb_lines)
 		new_map[i] = g()->map.map_data[i];
-	while (i < nb_lines + 2)
+	while (i < nb_lines + 3)
 		new_map[i++] = NULL;
 	free(g()->map.map_data);
 	g()->map.map_data = new_map;
